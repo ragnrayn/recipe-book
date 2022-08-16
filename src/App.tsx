453ca72a8d0f5862c112ac517842b2b522b81361
ragnrayn <ragnrayn@gmail.com>
2022-08-16 15:12:45 +0300

    created search bar by title

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,18 @@ import { recipeService } from './services/recipe';
 function App() {
 
   const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState('');
+
+  const getSearchText = (e: any) => {
+    setSearch(e.target.value);
+  }
+
+  const searchByTitle = () => {
+    if (!search) {
+      recipeService.get().then((data) => setRecipes(data));
+    }
+    setRecipes(recipes.filter((recipe: any) => recipe.title.startsWith(search)));
+  }
 
   useEffect(() => {
     recipeService.get().then((data) => setRecipes(data));
@@ -13,25 +25,33 @@ function App() {
 
   return (
     <div className="App">
+      <div className="navbar">
+        <div className="container">
+          <div className="navbar__search">
+            <input type="text" onChange={(e) => getSearchText(e)} placeholder='Search' className='search__bar' />
+            <button type='button' onClick={() => searchByTitle()} >Search</button>
+          </div>
+        </div>
+      </div>
       <div className="container">
         <div className="cards">
           {
             recipes.map((recipe: any, index: number) => {
-              if (index < 10) {
+              if (index) {
                 return (
                   <div key={index} className="card__main">
                     <div className='card'>
                       <div className="card__title">
-                        { recipe.title }
+                        {recipe.title}
                       </div>
                       <div className="card__source">
-                        Source: { recipe.source }
+                        Source: {recipe.source}
                       </div>
                       <div className="card__category">
-                        Category: { recipe.category }
+                        Category: {recipe.category}
                       </div>
                       <div className="card__url">
-                        <a href={ recipe.url } target={'_blank'}>Go to recipe</a>
+                        <a href={recipe.url} target={'_blank'}>Go to recipe</a>
                       </div>
                     </div>
                   </div>
